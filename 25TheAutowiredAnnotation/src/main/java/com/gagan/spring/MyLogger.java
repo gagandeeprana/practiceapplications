2fab57220d157df6006b77f3189ddd1716d2be31
Gagandeep Singh <gagandeep.rana@cloudsmartz.net>
2016-07-22 18:54:40 +0530

    Commit-O

--- 25TheAutowiredAnnotation/src/main/java/com/gagan/spring/MyLogger.java
+++ 25TheAutowiredAnnotation/src/main/java/com/gagan/spring/MyLogger.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyLogger {
 
-	
-	@Autowired
+	@Autowired(required = false)
 	private MyConsoleWriter myConsoleWriter;
-	
+
 	@Autowired
 	private MyFileWriter myFileWriter;
 
+	
+	
 	public void setMyConsoleWriter(MyConsoleWriter myConsoleWriter) {
 		this.myConsoleWriter = myConsoleWriter;
 	}
@@ -20,6 +21,7 @@ public class MyLogger {
 	}
 
 	public void writeConsole(String text) {
+		if(myConsoleWriter!=null)
 		myConsoleWriter.write(text);
 	}
 
